1d7483ecb753ec8df5ed4c3fbde61a3b85b3c857
Kristen Papke <klpapke215@gmail.com>
2018-05-31 20:43:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == nil || board[index] == ""
     false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
-    false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" || board[index] == "O"
     true
     end
 end
