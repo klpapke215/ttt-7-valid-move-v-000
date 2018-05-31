7b8cf3bb35024ef46660b7218b01b35fa98f830d
Kristen Papke <klpapke215@gmail.com>
2018-05-31 19:58:42 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
   if board[index] == (0 - 8) && board[index] == " " || "  "
   true
-elsif board[index] == < 0 && > 8 || board[index] == "X" || "O"
+elsif board[index] == (< 0 && > 8) || board[index] == "X" || "O"
   false
   end
 end
